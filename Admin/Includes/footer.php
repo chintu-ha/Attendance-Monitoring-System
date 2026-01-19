dc18db80d104e13a08b3b584d8a12bd5886012c3
sahoomanoranjan747@gmail.com <sahoomanoranjan747@gmail.com>
2026-01-19 19:34:57 +0530

    initial commit

---
+++ Admin/Includes/footer.php
@@ -0,0 +1,9 @@
+ <footer class="sticky-footer bg-white" style="margin-top: 17%;">
+        <div class="container my-auto mx-80">
+          <div class="copyright text-center my-auto">
+            <span> &copy; <script> document.write(new Date().getFullYear()); </script> - Developed by manoranjan sahoo and the team -
+              STUDENT ATTENDANCE MANAGEMENT SYSTEM
+            </span>
+          </div>
+        </div>
+      </footer>
